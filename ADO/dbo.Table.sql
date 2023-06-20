56937813874ef3046eb6c9328fb19481a0bf1efe
Jacob Dobrinsky <105935014+masamoe@users.noreply.github.com>
2023-06-20 12:13:27 -0700

    ff

---
+++ ADO/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Price] MONEY NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [AddedOn] DATE NOT NULL DEFAULT GETDATE()
+)
+
